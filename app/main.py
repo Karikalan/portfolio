b323e57cfbd2779ee3ff9199e3c3e67e86354ebd
V.Deepak <karikalan2838@gmail.com>
2025-12-20 00:00:02 +0530

    Re-add contact router and remove main execution
    Re-add contact router and remove main execution block.

--- app/main.py
+++ app/main.py
@@ -20,7 +20,3 @@ app.include_router(home.router)
 app.include_router(about.router)
 app.include_router(projects.router)
 app.include_router(contact.router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
